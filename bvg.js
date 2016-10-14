785f3ce2c8ffdcd0c5298eb1471f4407458b141f
ld <laurent.dorier@dpd.com>
2016-10-14 13:29:50 +0200

    Initial release

---
+++ bvg.js
@@ -0,0 +1,34 @@
+/*
+ Theme Name:   BVG
+ Theme URI:    http://lignoplan.com/wordpress_9
+ Description:  Vantage Child Theme für den BVG Verein
+ Author:       Laurent Dorier
+ Author URI:   http://etalkers.org
+ Template:     vantage
+ Version:      1.0.0
+ License:      GNU General Public License v2 or later
+ License URI:  http://www.gnu.org/licenses/gpl-2.0.html
+ Tags:         light, dark, two-columns, right-sidebar, responsive-layout, accessibility-ready
+ Text Domain:  vantage
+*/
+
+/*
+Doc child theme
+https://codex.wordpress.org/Child_Themes#How_to_Create_a_Child_Theme
+File called from the bvg_widgets plugin
+*/
+console.log( 'JS File for BVG ok' );
+
+
+jQuery( document ).on( 'ready' , function(){
+    console.log( 'Los...' );
+    // Sidebar Slidedown effect
+    jQuery( '.widget-area aside h3' ).on( 'mouseover' , function(){
+        if( jQuery( this ).next().css( 'display' ) === 'none' ){
+            jQuery( '.widget-area aside ul' ).slideUp();
+            jQuery( this ).next().slideDown();
+        }
+
+    });
+
+});
--- functions.php
+++ functions.php
@@ -7,9 +7,35 @@
  * @license GPL 2.0
  */
 
-// Include Vantage
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+// Include CSS
 function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+
+// Include JS
+function my_theme_enqueue_js() {
+    wp_enqueue_script( 'bvg-script', get_stylesheet_directory_uri() . '/bvg.js' );
+
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_js' );
+
+
+// Custom HTML Head
+function bvg_custom_head(){
+    //echo '<meta name="description" content="BVG Goldbach-Laufach Badminton Verein Webseite (Aschaffenburg Sport und Hobby)" />';
+
+    echo '<meta name="description" content="';
+    bloginfo('name');
+    echo " - ";
+
+    if( is_single() ){
+        single_post_title('', true);
+    }else{
+        bloginfo('description');
+    }
+    echo '" />';
+}
+add_action('wp_head', 'bvg_custom_head', 1);
