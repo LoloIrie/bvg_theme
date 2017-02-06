24a30105c921fd53ff55ebb12883464ff8f5c6da
ld <laurent.dorier@dpd.com>
2017-02-06 14:42:22 +0100

    Initial release

--- header.php
+++ header.php
@@ -11,11 +11,11 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta http-equiv="X-UA-Compatible" content="IE=10" />
-	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta http-equiv="X-UA-Compatible" content="IE=10" />
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -26,20 +26,20 @@
 
 <div id="page-wrapper">
 
-	<?php do_action( 'vantage_before_masthead' ); ?>
+    <?php do_action( 'vantage_before_masthead' ); ?>
 
-	<?php if( ! siteorigin_page_setting( 'hide_masthead', false ) ) : ?>
+    <?php if( ! siteorigin_page_setting( 'hide_masthead', false ) ) : ?>
 
-		<?php get_template_part( 'parts/masthead', apply_filters( 'vantage_masthead_type', siteorigin_setting( 'layout_masthead' ) ) ); ?>
+        <?php get_template_part( 'parts/masthead', apply_filters( 'vantage_masthead_type', siteorigin_setting( 'layout_masthead' ) ) ); ?>
 
-	<?php endif; ?>
+    <?php endif; ?>
 
-	<?php do_action( 'vantage_after_masthead' ); ?>
+    <?php do_action( 'vantage_after_masthead' ); ?>
 
-	<?php vantage_render_slider() ?>
+    <?php vantage_render_slider() ?>
 
-	<?php do_action( 'vantage_before_main_container' ); ?>
+    <?php do_action( 'vantage_before_main_container' ); ?>
 
-	<div id="main" class="site-main">
-		<div class="full-container">
-			<?php do_action( 'vantage_main_top' ); ?>
+    <div id="main" class="site-main">
+        <div class="full-container">
+            <?php do_action( 'vantage_main_top' ); ?>
